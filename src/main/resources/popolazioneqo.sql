bd9d6af46956abbefe9939595233368454f7d79b
mariacaterinadaloia <83784491+mariacaterinadaloia@users.noreply.github.com>
2023-03-07 22:33:55 +0100

    Create popolazioneqo.sql

---
+++ src/main/resources/popolazioneqo.sql
@@ -0,0 +1,92 @@
+/*popolazione*/
+/*avviare dopo aver avviato quartumopus.sql
+create table utente(
+	mail varchar(256) not null, 
+    password varchar(45) not null, 
+    nome varchar(45) not null, 
+    cognome varchar(60) not null, 
+    datadinascita date not null, 
+    gestore bool default false, 
+    primary key(mail)
+    ); */
+    use quartumopus; 
+    
+    insert into utente values ('catedal@gmail.com', 'machitsap', 'Kate', 'Dal', 20010706, true); 
+    insert into utente values ('framon@gmail.com', 'hetconlamama', 'Francies',  'Monets', 20010505 , true); 
+    insert into utente values ('manuilmagnifico@hotmail.com', 'Magnifique!3', 'Manuel', 'Sicà', 20010526, false); 
+    insert into utente values('ludssss@gmail.com', 'ZLmylife', 'Ludo', 'Genovens', 20010716, false); 
+    
+/*create table ordine(
+	id int not null auto_increment, 
+    data date not null, 
+    utente varchar(256) not null, 
+    primary key(id), 
+    foreign key(utente) references utente(mail)
+);*/
+
+insert into ordine values (1, 20200228, 'ludssss@gmail.com'); 
+insert into ordine values (2, 20200304, 'manuilmagnifico@hotmail.com'); 
+insert into ordine values (3, 20220109, 'ludssss@gmail.com'); 
+/*
+create table autore(
+	nome varchar(45) not null, 
+    cognome varchar(60) not null, 
+    codice varchar(16) not null, 
+    datanascita date not null, 
+    primary key(codice)
+);
+*/
+
+insert into autore values ('Julia', 'Queen', '934723', 19901230); 
+insert into autore values ('Andrea', 'Queen', '956723', 19901230); 
+insert into autore values ('Nova', 'Queen', '934423', 19901230);
+/*
+create table prodotto(
+	ISBN bigint not null, 
+    nome varchar(512) not null, 
+    genere varchar(32) not null, 
+    anno int not null, 
+    edizione int not null, 
+    casa_editrice varchar(128) not null, 
+    copertina varchar(512),
+    primary key(ISBN)
+); */
+
+insert into prodotto values (093764321, 'I tuoi occhi mi parlano...',  'romanzo', 2020, 3, 'Einadi', 'StringCopertina'); 
+insert into prodotto values (98483645, '.., e mi dicono...', 'thriller', 2008, 4, 'Feltrinelli', 'String'); 
+insert into prodotto values (2844872, '...hai paura di IS', 'giallo', 2011, 1,  'Mondadori', 'strgg'); 
+/*
+create table effettuato_da(
+	ordine int not null, 
+    utente varchar(256) not null, 
+    foreign key(utente) references utente(mail), 
+    foreign key(ordine) references ordine(id)
+);*/
+insert into effettuato_da values (1, 'ludssss@gmail.com'); 
+insert into effettuato_da values (2, 'manuilmagnifico@hotmail.com'); 
+insert into effettuato_da values (3, 'ludssss@gmail.com'); 
+/*
+create table scritto_da(
+	prodotto bigint not null, 
+    autore varchar(16) not null, 
+    foreign key(autore) references autore(codice), 
+    foreign key(prodotto) references prodotto(ISBN)
+);
+*/
+
+insert into scritto_da values (093764321, '934723'); 
+insert into scritto_da values (98483645, '956723'); 
+insert into scritto_da values (2844872, '934423');
+
+/*
+create table contiene(
+	ordine int not null, 
+    foreign key(ordine) references ordine(id),
+    prodotto bigint not null, 
+	foreign key(prodotto) references prodotto(ISBN)
+);*/
+
+insert into contiene values (1,093764321); 
+insert into contiene values (1, 98483645); 
+insert into contiene values (2, 98483645); 
+insert into contiene values (3, 98483645); 
